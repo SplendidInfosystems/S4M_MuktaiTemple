fbdcc7bbd2f89df79e8b1e562977d9b9ca79f70f
akshadaghule3 <akshadaghule3798@gmail.com>
2025-07-23 18:37:31 +0530

    validation in form

--- muktaiTempleProject/src/app/features/dashboard/add-donor/add-donor.component.ts
+++ muktaiTempleProject/src/app/features/dashboard/add-donor/add-donor.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-donor',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './add-donor.component.html',
   styleUrl: './add-donor.component.css'
 })
 export class AddDonorComponent {
+    donorForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.donorForm = this.fb.group({
+      donorName: ['', Validators.required],
+      date: ['', Validators.required],
+      address: ['', Validators.required],
+      mobileNo: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+      amount: ['', [Validators.required, Validators.min(1)]],
+      paymentMethod: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.donorForm.valid) {
+      console.log(this.donorForm.value);
+      // handle form submit
+    } else {
+      this.donorForm.markAllAsTouched();
+    }
+  }
+   
 }
--- muktaiTempleProject/src/app/features/dashboard/add-expences/add-expences.component.ts
+++ muktaiTempleProject/src/app/features/dashboard/add-expences/add-expences.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-expences',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './add-expences.component.html',
-  styleUrl: './add-expences.component.css'
+  styleUrls: ['./add-expences.component.css']
 })
 export class AddExpencesComponent {
+  expenseForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.expenseForm = this.fb.group({
+      expenseName: ['', Validators.required],
+      date: ['', Validators.required],
+      category: ['', Validators.required],
+      amount: [null, [Validators.required, Validators.min(1)]],
+      description: ['', Validators.required],
+      state: ['', Validators.required]
+    });
+  }
+    onSubmit() {
+    if (this.expenseForm.valid) {
+      console.log(this.expenseForm.value);
+      // handle form submit
+    } else {
+      this.expenseForm.markAllAsTouched();
+    }
+  }
 }
