ce3233cee5b117c7447a34e89ba7803017609391
ghuleAkshada3798 <akshada.ghule@splendidinfosystems.com>
2026-01-20 18:25:32 +0530

    new project changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path:'home',
+        component:HomeComponent
+
+    },
+
+
+    {
+        path: 'header',
+        component: HeaderComponent
+    },
+
+];
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#D4AF37",
+        templeOrange: "#FF6B35",
+      },
+    },
+  },
+  plugins: [],
+};
