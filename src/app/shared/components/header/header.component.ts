1c383c4294f00adf0a3144d79728a4f16d76638e
ghuleAkshada3798 <akshada.ghule@splendidinfosystems.com>
2026-01-23 19:35:07 +0530

    about us in home page done

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
    menuOpen = false;
 
+  currentLang: 'mr' | 'en' = 'mr';
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('mr');
+    this.translate.use('mr');
+  }
+
+  toggleLanguage() {
+    this.currentLang = this.currentLang === 'mr' ? 'en' : 'mr';
+    this.translate.use(this.currentLang);
+  }
 }
--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+currentLang: 'mr' | 'en' = 'mr';
+
+  constructor(private translate: TranslateService) {
+  
+  }
+
 
 }
