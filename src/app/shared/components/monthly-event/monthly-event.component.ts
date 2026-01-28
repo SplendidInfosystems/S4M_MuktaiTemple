6f09dc83b42c9fadcc73daf5f0ddd0c31a1ba02c
ghuleAkshada3798 <akshada.ghule@splendidinfosystems.com>
2026-01-28 16:35:47 +0530

    monthly event page done 28/01/2025

--- src/app/shared/components/monthly-event/monthly-event.component.ts
+++ src/app/shared/components/monthly-event/monthly-event.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-monthly-event',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './monthly-event.component.html',
   styleUrl: './monthly-event.component.css'
 })
